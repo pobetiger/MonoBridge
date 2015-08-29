eb5db9062a4b143ad8d2cbef2de866e628b91f2c
pobetiger <pobetiger@gmail.com>
2015-08-28 21:56:46 -0500

    lib/FileIOLib: added features to test library

--- lib/FileIOLib.cs
+++ lib/FileIOLib.cs
@@ -4,12 +4,32 @@ using System;
 
 namespace MonoBridgeTest {
     public class FileIOLib : IDisposable {
+
+        string myport;
+        string mydesc;
+
         public FileIOLib() {
             System.Console.WriteLine("FileIOLib created");
+            myport = "unknown";
+            mydesc = "default";
         }
 
+        public void Setup(string port) {
+
+            System.Console.WriteLine(string.Format("Setup to use port: {0}", port));
+            myport = port;
+
+        }
+
+        public void Setup(string port, string desc) {
+            System.Console.WriteLine(string.Format("Setup to use port: {0} for {1}", port, desc));
+            myport = port;
+            mydesc = desc;
+        }
+
+
         public void Dispose() {
-            System.Console.WriteLine("FileIOLib disposed");
+            System.Console.WriteLine(string.Format("FileIOLib port={0} disposed", myport));
         }
 
     }
