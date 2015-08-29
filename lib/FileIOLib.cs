9831f673a84b59696b7a906198e528d80820a7d6
pobetiger <pobetiger@gmail.com>
2015-08-28 23:35:19 -0500

    Modified FileIOLIb to simulate some traffic

--- lib/FileIOLib.cs
+++ lib/FileIOLib.cs
@@ -15,10 +15,8 @@ namespace MonoBridgeTest {
         }
 
         public void Setup(string port) {
-
             System.Console.WriteLine(string.Format("Setup to use port: {0}", port));
             myport = port;
-
         }
 
         public void Setup(string port, string desc) {
@@ -27,6 +25,15 @@ namespace MonoBridgeTest {
             mydesc = desc;
         }
 
+        public string getDesc() {
+            return mydesc;
+        }
+
+        public void Write(string packet) {
+            System.Threading.Thread.Sleep(5); // wait .5 second
+            System.Console.WriteLine("\t\t\t>>> Simulating some IO delay and packet outgoing");
+            System.Console.WriteLine("\t\t\t>>> Data written: " + packet);
+        }
 
         public void Dispose() {
             System.Console.WriteLine(string.Format("FileIOLib port={0} disposed", myport));
